dc9530eb24d24fc372c7d17a656ae8d704930b0f
Christopher Prener <chris.prener@slu.edu>
2020-04-02 07:24:41 -0500

    add population estimates for city zips

--- source/archive/build_zips.R
+++ source/archive/build_zips.R
@@ -1,6 +1,7 @@
 # download demographic data
 
 # dependencies
+library(areal)
 library(dplyr)
 library(readr)
 library(sf)
@@ -75,9 +76,41 @@ st_write(region, "data/source/stl_zips/region_zip/region_zip.shp", delete_dsn = 
 st_write(region, "data/source/stl_zips/region_zip/region_zip.geojson", delete_dsn = TRUE)
 
 # get tract data
-get_acs(year = 2018, geography = "zcta", variables = "B01003_001") %>%
+total_pop <- get_acs(year = 2018, geography = "zcta", variables = "B01003_001") %>%
   select(GEOID, estimate) %>%
-  filter(GEOID %in% region$zip) %>%
+  filter(GEOID %in% stl_city$zip) %>%
   rename(total_pop = estimate)
 
-get_acs(state = 29, county = 510, year = 2018, geography = "tract", variables = "B01003_001")
+poverty <- get_acs(year = 2018, geography = "zcta", table = "B17001", output = "wide") %>%
+  select(GEOID, B17001_001E, B17001_002E) %>%
+  filter(GEOID %in% stl_city$zip) 
+
+race <- get_acs(year = 2018, geography = "zcta", table = "B02001", output = "wide") %>%
+  select(GEOID, B02001_001E, B02001_002E, B02001_003E) %>%
+  filter(GEOID %in% stl_city$zip) 
+
+zip %>%
+  filter(GEOID10 %in% stl_city$zip) %>%
+  select(GEOID10) %>%
+  rename(GEOID = GEOID10) %>%
+  st_transform(crs = 26915) %>%
+  left_join(., total_pop, by = "GEOID") %>%
+  left_join(., poverty, by = "GEOID") %>%
+  left_join(., race, by = "GEOID") -> stl_city_demo
+
+# interpolate
+stl_city %>%
+  aw_interpolate(tid = "zip", source = stl_city_demo, sid = "GEOID", weight = "sum", output = "sf", 
+                 extensive = c("total_pop", "B17001_001E", "B17001_002E",
+                               "B02001_001E", "B02001_002E", "B02001_003E")) %>%
+  mutate(total_pop = round(total_pop)) %>%
+  mutate(pvty_pct = B17001_002E/B17001_001E*100) %>%
+  mutate(wht_pct = B02001_002E/B02001_001E*100) %>%
+  mutate(blk_pct = B02001_003E/B02001_001E*100) %>%
+  select(zip, total_pop, pvty_pct, wht_pct, blk_pct) -> stl_city
+
+st_write(stl_city, "data/source/stl_zips/stl_city_zip/stl_city_zip.shp", delete_dsn = TRUE)
+
+stl_city %>%
+  st_transform(crs = 4326) %>%
+  st_write("data/source/stl_zips/stl_city_zip/stl_city_zip.geojson", delete_dsn = TRUE)
