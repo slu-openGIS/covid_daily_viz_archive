a8e0b4a2ac3f46f3b97087bd67305d8233120d30
Christopher Prener <chris.prener@slu.edu>
2020-05-12 19:28:37 -0500

    build local data for 2020-05-12

--- source/_build.R
+++ source/_build.R
@@ -29,14 +29,6 @@ source("source/workflow/build/05_create_zip.R")
 source("source/workflow/build/06_create_testing.R")
 source("source/workflow/build/07_create_stl_hospital.R")
 
-# update interactive map
-rmarkdown::render(input = "docs/index.Rmd",
-                  params = list(
-                    date = paste0("Current as of ", as.character(date)),
-                    date_val = as.character(date),
-                    prior_date_val = as.character(date-7)
-                  ))
-
 # update README
 rmarkdown::render(input = "README.Rmd",
                   params = list(
@@ -47,6 +39,3 @@ rmarkdown::render(input = "README.Rmd",
 fs::file_delete("README.html")
 
 # clean-up
-rm(pal, snapshot, date, zip_snapshot, map_breaks, map_bins, bins, round_any,
-   data_table, state_data, stl_city_data, stl_county_data, kc_city_data,
-   kc_metro_data, stl_metro_data, state_test_data)
---
+++ source/_build_pm.R
@@ -0,0 +1,26 @@
+# Scape Data and Construct Data Sets, PM only
+
+# UPDATE date value
+date <- lubridate::mdy("05-12-2020")
+
+# UPDATE city zip's with < 5 or 0 cases
+city_lt5 <- c("63105", "63117", "63119", "63125", "63130", "63133", "63137", "63143")
+
+# dependencies
+library(dplyr)
+library(lubridate)
+library(purrr)
+library(readr)
+library(sf)
+library(zoo)
+
+# functions
+source("source/functions/wrangle_zip.R")
+
+# workflow
+source("source/workflow/build/05_create_zip.R")
+source("source/workflow/build/06_create_testing.R")
+source("source/workflow/build/07_create_stl_hospital.R")
+
+# clean-up
+rm(date)
