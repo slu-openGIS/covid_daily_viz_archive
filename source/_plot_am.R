d44e48710506dc0a9bfbbf14177e0d2b560a71d7
Christopher Prener <chris.prener@slu.edu>
2020-05-16 08:00:43 -0500

    build am plots for 2020-05-15

--- source/_plot_am.R
+++ source/_plot_am.R
@@ -3,7 +3,7 @@
 # =============================================================================
 
 # UPDATE date value
-date <- lubridate::mdy("05-14-2020")
+date <- lubridate::mdy("05-15-2020")
 
 # define first date for plotting
 plot_date <- "2020-03-10"
