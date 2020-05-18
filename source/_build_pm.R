441b0391f10a04244f97ee0235275f55963a65b7
Christopher Prener <chris.prener@slu.edu>
2020-05-17 19:46:38 -0500

    build pm data for 2020-05-17

--- source/_build_pm.R
+++ source/_build_pm.R
@@ -1,10 +1,11 @@
 # Scape Data and Construct Data Sets, PM only
 
 # UPDATE date value
-date <- lubridate::mdy("05-16-2020")
+# date <- lubridate::mdy("05-16-2020")
+date <- Sys.Date()
 
 # UPDATE city zip's with < 5 or 0 cases
-city_lt5 <- c("63105", "63117", "63119", "63125", "63130", "63133", "63137", "63143")
+city_lt5 <- c("63105", "63117", "63119", "63125", "63130", "63133", "63143")
 
 # dependencies
 library(dplyr)
