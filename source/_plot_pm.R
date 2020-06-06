83540428e29127b72c685c0d19ccfb6121f92d41
Christopher Prener <chris.prener@slu.edu>
2020-06-06 19:41:42 -0400

    build pm data for 2020-06-06

--- source/_plot_pm.R
+++ source/_plot_pm.R
@@ -11,8 +11,8 @@ test_date_breaks <- "5 days"
 new_pt_breaks <- "8 days"
 
 # create caption
-# caption_text_tests <- "Plot by Christopher Prener, Ph.D.\nData via The Atlantic COVID-19 Tracking Project and the State of Missouri"
-# caption_text_tests_census <- "Plot by Christopher Prener, Ph.D.\nData via The Atlantic COVID-19 Tracking Project, the State of Missouri, and and the U.S. Census Bureau"
+caption_text_tests <- "Plot by Christopher Prener, Ph.D.\nData via The Atlantic COVID-19 Tracking Project and the State of Missouri"
+caption_text_tests_census <- "Plot by Christopher Prener, Ph.D.\nData via The Atlantic COVID-19 Tracking Project, the State of Missouri, and and the U.S. Census Bureau"
 
 # =============================================================================
 
