228ebf0817e9c871748f80528545db0a33536006
Christopher Prener <chris.prener@slu.edu>
2020-07-06 12:02:53 -0400

    build am plots for 2020-07-05

--- source/workflow/plot/04_regional_plots.R
+++ source/workflow/plot/04_regional_plots.R
@@ -6,11 +6,14 @@
 ## plot values
 report_label_x <- -15
 report_label_y <- 70
-peak_point_x <- 0
-peak_point_y <- 125
-current_point_x <- -35
-current_point_y <- -35
-
+# peak_point_x <- 0
+# peak_point_y <- 125
+# current_point_x <- -35
+# current_point_y <- -35
+peak_point_x <- -35
+peak_point_y <- -35
+current_point_x <- 0
+current_point_y <- 125
 
 ## data values
 stl <- c("29071", "29099", "29113", "29183", "29189", "29219", "29510")
@@ -124,7 +127,7 @@ p <- ggplot() +
   geom_text_repel(data = peak_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
                   nudge_y = -135, nudge_x = 18, size = 5) +
   geom_text_repel(data = current_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
-                  nudge_y = -130, nudge_x = -1, size = 5) +
+                  nudge_y = -120, nudge_x = -1, size = 5) +
   scale_color_brewer(palette = "Dark2", name = "Category") +
   scale_y_continuous(limits = c(0, 500), breaks = seq(0, 500, by = 50)) +
   scale_x_date(date_breaks = date_breaks_alt, date_labels = "%d %b") +
@@ -255,8 +258,8 @@ p <- ggplot() +
                   nudge_y = current_point_y, nudge_x = current_point_x, size = 5) +
   geom_text_repel(data = peak_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
                   nudge_y = 25, nudge_x = -1, size = 5) +
-  # geom_text_repel(data = current_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
-  #                nudge_y = -80, nudge_x = -1, size = 5) +
+  geom_text_repel(data = current_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
+                  nudge_y = -100, nudge_x = -1, size = 5) +
   scale_color_brewer(palette = "Dark2", name = "Category") +
   scale_y_continuous(limits = c(0, 500), breaks = seq(0, 500, by = 50)) +
   scale_x_date(date_breaks = date_breaks_alt, date_labels = "%d %b") +
