bdc1c0cf38271a632c855be391de0b4dbdfc33f2
Christopher Prener <chris.prener@slu.edu>
2020-07-07 19:15:37 -0400

    build pm plots for 2020-07-07

--- source/workflow/plot/16_stl_individual_plots.R
+++ source/workflow/plot/16_stl_individual_plots.R
@@ -23,13 +23,13 @@ covid_race_gender %>%
 # plot morbidity rates, race
 
 ## define top_val
-top_val <- round_any(x = max(covid_race$case_rate, na.rm = TRUE), accuracy = .5, f = ceiling)
+top_val <- round_any(x = max(covid_race$case_rate, na.rm = TRUE), accuracy = 1, f = ceiling)
 
 ## create plot
 p <- ggplot(data = covid_race, mapping = aes(x = value, y = case_rate, fill = county)) +
   geom_bar(position = "dodge", stat = "identity") +
   scale_fill_brewer(palette = "Set1", name = "County") +
-  scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = .5)) +
+  scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = 1)) +
   labs(
     title = "Reported Cases by Race, St. Louis City & County",
     subtitle = paste0("Current as of ", as.character(date)),
@@ -50,7 +50,7 @@ save_plots(filename = "results/low_res/stl_individual/a_race_case.png", plot = p
 # plot mortality rates, race
 
 ## define top_val
-top_val <- round_any(x = max(covid_race$mortality_rate, na.rm = TRUE), accuracy = .05, f = ceiling)
+top_val <- round_any(x = max(covid_race$mortality_rate, na.rm = TRUE), accuracy = .1, f = ceiling)
 
 ## create plot
 p <- covid_race %>%
@@ -58,7 +58,7 @@ p <- covid_race %>%
   ggplot(data = ., mapping = aes(x = value, y = mortality_rate, fill = county)) +
     geom_bar(position = "dodge", stat = "identity", width = .75) +
     scale_fill_brewer(palette = "Set1", name = "County") +
-    scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = .05)) +
+    scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = .1)) +
     labs(
       title = "Reported Deaths by Race, St. Louis City & County",
       subtitle = paste0("Current as of ", as.character(date)),
@@ -77,13 +77,13 @@ save_plots(filename = "results/low_res/stl_individual/b_race_mortality.png", plo
 # plot morbidity rates, race and gender
 
 ## define top_val
-top_val <- round_any(x = max(covid_race_gender$case_rate, na.rm = TRUE), accuracy = .5, f = ceiling)
+top_val <- round_any(x = max(covid_race_gender$case_rate, na.rm = TRUE), accuracy = 1, f = ceiling)
 
 ## create plot
 p <- ggplot(data = covid_race_gender, mapping = aes(x = value, y = case_rate, fill = sex)) +
   geom_bar(position = "dodge", stat = "identity", width = .75) +
   scale_fill_brewer(palette = "Set1", name = "Sex") +
-  scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = .5)) +
+  scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = 1)) +
   labs(
     title = "Reported Cases by Race and Sex, St. Louis City",
     subtitle = paste0("Current as of ", as.character(date)),
@@ -102,7 +102,7 @@ save_plots(filename = "results/low_res/stl_individual/c_race_sex_case.png", plot
 # plot morbidity rates, race and gender
 
 ## define top_val
-top_val <- round_any(x = max(covid_race_gender$mortality_rate, na.rm = TRUE), accuracy = .05, f = ceiling)
+top_val <- round_any(x = max(covid_race_gender$mortality_rate, na.rm = TRUE), accuracy = .1, f = ceiling)
 
 ## create plot
 p <- covid_race_gender %>%
@@ -110,7 +110,7 @@ p <- covid_race_gender %>%
   ggplot(., mapping = aes(x = value, y = mortality_rate, fill = sex)) +
     geom_bar(position = "dodge", stat = "identity", width = .75) +
     scale_fill_brewer(palette = "Set1", name = "Sex") +
-    scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = .05)) +
+    scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = .1)) +
     labs(
       title = "Reported Deaths by Race and Sex, St. Louis City",
       subtitle = paste0("Current as of ", as.character(date)),
