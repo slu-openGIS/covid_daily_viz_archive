28a8bfc4375a8d09cc7b22ccea49ef86e810925c
Christopher Prener <chris.prener@slu.edu>
2020-07-07 14:36:20 -0400

    build am plots for 2020-07-06

--- source/workflow/plot/04_regional_plots.R
+++ source/workflow/plot/04_regional_plots.R
@@ -258,8 +258,8 @@ p <- ggplot() +
                   nudge_y = current_point_y, nudge_x = current_point_x, size = 5) +
   geom_text_repel(data = peak_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
                   nudge_y = 25, nudge_x = -1, size = 5) +
-  geom_text_repel(data = current_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
-                  nudge_y = -100, nudge_x = -1, size = 5) +
+  # geom_text_repel(data = current_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
+  #                nudge_y = -100, nudge_x = -1, size = 5) +
   scale_color_brewer(palette = "Dark2", name = "Category") +
   scale_y_continuous(limits = c(0, 500), breaks = seq(0, 500, by = 50)) +
   scale_x_date(date_breaks = date_breaks_alt, date_labels = "%d %b") +
@@ -397,7 +397,7 @@ p <- ggplot() +
   geom_text_repel(data = current_point, mapping = aes(x = report_date, y = case_avg, label = text),
                   nudge_y = current_point_y, nudge_x = current_point_x, size = 5) +
   geom_text_repel(data = peak_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
-                  nudge_y = -100, nudge_x = -35, size = 5) +
+                  nudge_y = -100, nudge_x = -40, size = 5) +
   geom_text_repel(data = current_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
                   nudge_y = -170, nudge_x = 0, size = 5) +
   scale_color_brewer(palette = "Dark2", name = "Category") +
