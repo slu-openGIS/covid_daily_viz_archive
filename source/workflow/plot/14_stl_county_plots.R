046929a8a6648acbde0d8e33dbd43f7d4070c325
Christopher Prener <chris.prener@slu.edu>
2020-07-14 13:00:46 -0400

    build am plots for 2020-07-13

--- source/workflow/plot/14_stl_county_plots.R
+++ source/workflow/plot/14_stl_county_plots.R
@@ -5,7 +5,7 @@
 # load data
 stl_sf <- st_read("data/metro/daily_snapshot_stl.geojson", crs = 4326,
                   stringsAsFactors = FALSE) %>%
-  st_transform(crs = 102003) %>%
+  st_transform(crs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs ") %>%
   mutate(county = ifelse(GEOID %in% c("29189"), NA, county))
 
 county_data <- read_csv("data/county/county_full.csv") %>%
