9cea375d16e66c638c0c53ff4f11a5b02d29180a
Christopher Prener <chris.prener@slu.edu>
2020-07-18 19:24:46 -0400

    re-arrange repo

--- source/archive/ratio.R
+++
@@ -1,7 +0,0 @@
-library(tidyr)
-
-state_subset %>%
-  select(-factor_var) %>%
-  mutate(state = ifelse(state == "Missouri (No STL)", "Missouri_nonSTL", state)) %>%
-  pivot_wider(names_from = "state", values_from = "case_avg") %>%
-  mutate(ratio = Missouri/Missouri_nonSTL) -> state_subset2
--- to-do-items.md
+++
@@ -1,3 +0,0 @@
-
-1. suppress reader output
-2. warnings on metro, semo, midmo, stl_county, stl_zip, 
