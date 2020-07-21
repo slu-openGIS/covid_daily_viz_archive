036c7c93a0b545ca72b4f45969be685911057d62
Christopher Prener <chris.prener@slu.edu>
2020-07-21 19:16:36 -0400

    build pm plots for 2020-07-21

--- source/_plot_pm.R
+++ source/_plot_pm.R
@@ -7,9 +7,9 @@
 date <- Sys.Date()
 
 # define first date for plotting
-test_date_breaks <- "3 days"
+test_date_breaks <- "7 days"
 total_test_date_breaks <- "10 days"
-hosp_breaks <- "7 days"
+hosp_breaks <- "9 days"
 x_angle <- 25
 
 # create caption
