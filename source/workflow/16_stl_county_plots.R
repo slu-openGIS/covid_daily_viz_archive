f5b8520c1b9264c19c1bc321441bc9f340236e37
Christopher Prener <chris.prener@slu.edu>
2020-07-27 16:21:35 -0400

    build am plots for 2020-07-26

--- source/workflow/16_stl_county_plots.R
+++ source/workflow/16_stl_county_plots.R
@@ -163,7 +163,7 @@ p <- ggplot(data = county_subset) +
   geom_text_repel(data = report_label, mapping = aes(x = day, y = cases, label = text),
                   nudge_y = .3, nudge_x = -1, size = 5) +
   scale_colour_manual(values = cols, name = "County") +
-  scale_y_log10(limits = c(5, 11000), breaks = c(5,10,30,100,300,1000,3000,10000),
+  scale_y_log10(limits = c(5, 12000), breaks = c(5,10,30,100,300,1000,3000,10000),
                 labels = comma_format(accuracy = 1)) +
   scale_x_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = 7)) +
   labs(
@@ -236,7 +236,7 @@ p <- ggplot() +
   geom_text_repel(data = report_label, mapping = aes(x = day, y = case_avg, label = text),
                   nudge_y = .3, nudge_x = -1, size = 5) +
   scale_colour_manual(values = cols, name = "County") +
-  scale_y_log10(limits = c(.1, 300), breaks = c(.1, .3, 1, 3, 10, 30, 100, 300), 
+  scale_y_log10(limits = c(.1, 1000), breaks = c(.1, .3, 1, 3, 10, 30, 100, 300, 1000), 
                 labels = comma_format(accuracy = .2)) +
   scale_x_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = 7)) +
   labs(
