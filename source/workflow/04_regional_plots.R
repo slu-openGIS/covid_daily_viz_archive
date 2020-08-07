7e131d808ccfcf1826fdfc91b21356104368bafc
Christopher Prener <chris.prener@slu.edu>
2020-08-07 19:34:36 -0400

    build am plots for 2020-08-06

--- source/workflow/04_regional_plots.R
+++ source/workflow/04_regional_plots.R
@@ -8,7 +8,7 @@ report_label_x <- 25
 report_label_y <- 80
 peak_point_x <- 0
 peak_point_y <- 125
-current_point_x <- -50
+current_point_x <- -60
 current_point_y <- -70
 # peak_point_x <- -35
 # peak_point_y <- -35
@@ -127,7 +127,7 @@ p <- ggplot() +
   geom_text_repel(data = peak_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
                   nudge_y = 70, nudge_x = -60, size = 5) +
   geom_text_repel(data = current_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
-                  nudge_y = -540, nudge_x = -1, size = 5) +
+                  nudge_y = -500, nudge_x = -1, size = 5) +
   scale_color_brewer(palette = "Dark2", name = "Category") +
   scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = 100)) +
   scale_x_date(date_breaks = date_breaks_alt, date_labels = "%d %b") +
@@ -256,7 +256,7 @@ p <- ggplot() +
   geom_text_repel(data = peak_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
                   nudge_y = 170, nudge_x = -70, size = 5) +
   geom_text_repel(data = current_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
-                  nudge_y = -300, nudge_x = 0, size = 5) +
+                  nudge_y = -250, nudge_x = 0, size = 5) +
   scale_color_brewer(palette = "Dark2", name = "Category") +
   scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = 100)) +
   scale_x_date(date_breaks = date_breaks_alt, date_labels = "%d %b") +
@@ -397,7 +397,7 @@ p <- ggplot() +
   geom_text_repel(data = peak_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
                   nudge_y = 80, nudge_x = -70, size = 5) +
   geom_text_repel(data = current_point_nostl, mapping = aes(x = report_date, y = case_avg, label = text),
-                  nudge_y = -360, nudge_x = 0, size = 5) +
+                  nudge_y = -320, nudge_x = 0, size = 5) +
   scale_color_brewer(palette = "Dark2", name = "Category") +
   scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = 100)) +
   scale_x_date(date_breaks = date_breaks_alt, date_labels = "%d %b") +
