b04f7dcfa2587336024b3f5d8c284941906593b9
Christopher Prener <chris.prener@slu.edu>
2020-09-10 19:19:54 -0400

    build pm plots for 2020-09-10

--- source/_plot_am.R
+++ source/_plot_am.R
@@ -72,6 +72,7 @@ source("source/workflow/12_county_plots_swmo.R")
 source("source/workflow/13_county_plots_cape.R")
 source("source/workflow/14_county_plots_west.R")
 source("source/workflow/15_county_plots_spring.R")
+source("source/workflow/16_county_plots_ozkmtns.R")
 source("source/workflow/17_stl_county_plots.R")
 source("source/workflow/21_kc_county_plots.R")
 
