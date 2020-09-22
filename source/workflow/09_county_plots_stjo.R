3c732c7b348b5a18a3cd43f97f27a1c32f39a545
Christopher Prener <chris.prener@slu.edu>
2020-09-22 19:40:35 -0400

    add county 7-day average plots

--- source/workflow/09_county_plots_stjo.R
+++ source/workflow/09_county_plots_stjo.R
@@ -9,11 +9,11 @@ county_data <- read_csv("data/MO_HEALTH_Covid_Tracking/data/county/county_full.c
 
 # =============================================================================
 
-# define colors
-pal <- brewer.pal(n = 8, name = "Set1")
-pal[6] <- "#FFD60C"
-cols <- c("St. Louis City" = pal[1], "St. Louis" = pal[2], "Kansas City" = pal[3],
-          "Andrew" = pal[4], "Buchanan" = pal[5], "DeKalb" = pal[8], "Clinton" = pal[6])
+# define cols object
+cols <- c("St. Louis City" = values$pal[1], "St. Louis" = values$pal[2], 
+          "Kansas City" = values$pal[3], "Andrew" = values$pal[4], 
+          "Buchanan" = values$pal[5], "DeKalb" = values$pal[8], 
+          "Clinton" = values$pal[6])
 
 # define focal metros
 county_focal <- c("29510", "29189", "29511", "29003", "29021", "29063", "29049")
@@ -22,33 +22,17 @@ county_focal <- c("29510", "29189", "29511", "29003", "29021", "29063", "29049")
 
 # create points
 ## create end points
-county_points <- filter(county_data, report_date == date) %>%
+county_points <- filter(county_data, report_date == values$date) %>%
   filter(geoid %in% county_focal)
-
-## create reporting change points
-report_points <- filter(county_data, report_date == as.Date("2020-04-15")) %>%
-  filter(geoid %in% county_focal) %>%
-  mutate(text = ifelse(geoid == "29189", "reporting change on 15 Apr", NA))
-
-# =============================================================================
-
-# create line label
-report_line <- tibble(
-  date = as.Date("2020-04-15"),
-  case_rate = county_rate_pos,
-  mortality_rate = NA,
-  case_fatality_rate = NA,
-  text = "reporting change on 15 Apr"
-)
 
 # =============================================================================
 
 # plot confirmed rate
 ## subset data
-county_subset <- filter(county_data, report_date >= plot_date)
+county_subset <- filter(county_data, report_date >= values$plot_date)
 
 ## define top_val
-top_val <- round_any(x = max(county_subset$case_rate), accuracy = county_rate_val, f = ceiling)
+top_val <- round_any(x = max(county_subset$case_rate), accuracy = values$county_rate_val, f = ceiling)
 
 ## create factors
 county_subset <- mutate(county_subset, factor_var = fct_reorder2(county, report_date, case_rate))
@@ -60,21 +44,19 @@ p <- ggplot() +
   geom_point(county_points, mapping = aes(x = report_date, y = case_rate, color = factor_var), 
              size = 4, show.legend = FALSE) +
   gghighlight(geoid %in% county_focal, use_direct_label = FALSE, use_group_by = FALSE) +
-  # geom_vline(xintercept = as.Date("2020-04-15"), linetype="dotted", size = 1.25) + 
-  # geom_text_repel(data = report_line, mapping = aes(x = date, y = case_rate, label = text),
-  #                nudge_y = county_rate_x, nudge_x = county_rate_y, size = 5) +
   scale_colour_manual(values = cols, name = "County") +
-  scale_x_date(date_breaks = date_breaks_alt, date_labels = "%d %b") +
-  scale_y_continuous(limits = c(0,top_val), breaks = seq(0, top_val, by = county_rate_val)) +  
+  scale_x_date(date_breaks = values$date_breaks, date_labels = "%d %b") +
+  scale_y_continuous(limits = c(0,top_val), breaks = seq(0, top_val, by = values$county_rate_val)) + 
   labs(
     title = "Reported COVID-19 Cases by Select Missouri Counties",
-    subtitle = paste0("St. Joseph Focus\n", as.character(plot_date), " through ", as.character(date)),
+    subtitle = paste0("St. Joseph Focus\n", as.character(values$plot_date), " through ", 
+                      as.character(values$date)),
     x = "Date",
     y = "Rate per 1,000",
-    caption = caption_text_census
+    caption = values$caption_text_census
   ) +
   sequoia_theme(base_size = 22, background = "white") +
-  theme(axis.text.x = element_text(angle = x_angle))
+  theme(axis.text.x = element_text(angle = values$x_angle))
 
 ## save plot
 save_plots(filename = "results/high_res/county_stjo/b_case_rate.png", plot = p, preset = "lg")
@@ -96,17 +78,9 @@ top_val <- round_any(x = max(county_subset$day), accuracy = 5, f = ceiling)
 ## identify max day
 county_subset %>%
   group_by(geoid) %>%
-  summarise(day = max(day)) %>%
+  summarise(day = max(day), .groups = "drop_last") %>%
   left_join(county_points, ., by = "geoid") %>%
   filter(county %in% unique(county_subset$county)) -> county_day_points
-
-## add day to report points
-county_subset %>%
-  select(county, report_date, day) %>%
-  left_join(report_points, ., by = c("county", "report_date")) %>%
-  filter(county %in% unique(county_subset$county)) -> report_day_points
-
-report_label <- filter(report_day_points, county == "St. Louis")
 
 ## create factors
 county_subset <- mutate(county_subset, factor_var = fct_reorder2(county, day, cases))
@@ -118,17 +92,14 @@ p <- ggplot(data = county_subset) +
   geom_point(county_day_points, mapping = aes(x = day, y = cases, color = factor_var), 
              size = 4, show.legend = FALSE) +
   gghighlight(geoid %in% county_focal, use_direct_label = FALSE, use_group_by = FALSE) +
-  # geom_point(report_day_points, mapping = aes(x = day, y = cases), size = 4, shape = 18) +
-  # geom_text_repel(data = report_label, mapping = aes(x = day, y = cases, label = text),
-  #                nudge_y = county_log_y, nudge_x = county_log_x, size = 5) +
   scale_colour_manual(values = cols, name = "County") +
-  scale_y_log10(limits = c(5, county_log_max), breaks = c(5,10,30,100,300,1000,3000,10000,30000),
+  scale_y_log10(limits = c(5, values$county_log_max), breaks = c(5,10,30,100,300,1000,3000,10000,30000), 
                 labels = comma_format(accuracy = 1)) +
-  scale_x_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = date_breaks_log)) +
+  scale_x_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = values$date_breaks_log)) +
   labs(
     title = "Pace of COVID-19 Cases by Select Missouri Counties",
-    subtitle = paste0("St. Joseph Focus\n", "Current as of ", as.character(date)),
-    caption = caption_text,
+    subtitle = paste0("St. Joseph Focus\n", "Current as of ", as.character(values$date)),
+    caption = values$caption_text,
     x = "Days Since Fifth Case Reported",
     y = "Count of Reported Cases (Log)"
   ) +
@@ -140,8 +111,45 @@ save_plots(filename = "results/low_res/county_stjo/c_case_log.png", plot = p, pr
 
 # =============================================================================
 
-# clean-up
-rm(county_data, county_subset, county_points, county_day_points, report_day_points, report_points, 
-   report_label, report_line, county_focal)
-rm(top_val, pal, cols, p)
+# plot confirmed rate
 
+## subset data
+county_subset <- filter(county_data, report_date >= values$plot_date) %>%
+  filter(geoid %in% county_focal)
+
+## define top_val
+top_val <- round_any(x = max(county_subset$case_avg_rate), accuracy = 10, f = ceiling)
+
+## create factors
+county_subset <- mutate(county_subset, factor_var = fct_reorder2(county, report_date, case_avg_rate))
+county_points <- mutate(county_points, factor_var = fct_reorder2(county, report_date, case_avg_rate))
+
+## create plot
+p <- ggplot(county_subset) +
+  geom_line(mapping = aes(x = report_date, y = case_avg_rate, color = factor_var), size = 2) +
+  geom_point(county_points, mapping = aes(x = report_date, y = case_avg_rate, color = factor_var), 
+             size = 4, show.legend = FALSE) +
+  gghighlight(geoid %in% county_focal, use_direct_label = FALSE, use_group_by = FALSE) +
+  scale_colour_manual(values = cols, name = "County") +
+  scale_x_date(date_breaks = values$date_breaks, date_labels = "%d %b") +
+  scale_y_continuous(limits = c(0,top_val), breaks = seq(0, top_val, by = 10)) + 
+  labs(
+    title = "Pace of New COVID-19 Cases in Select Missouri Counties",
+    subtitle = paste0("St. Joseph Focus\n",as.character(values$plot_date), " through ", as.character(values$date)),
+    x = "Date",
+    y = "7-Day Average Rate per 100,000",
+    caption = values$caption_text_census
+  ) +
+  sequoia_theme(base_size = 22, background = "white") +
+  theme(axis.text.x = element_text(angle = values$x_angle))
+
+## save plot
+save_plots(filename = "results/high_res/county_stjo/e_new_case.png", plot = p, preset = "lg")
+save_plots(filename = "results/low_res/county_stjo/e_new_case.png", plot = p, preset = "lg", dpi = 72)
+
+# =============================================================================
+
+# clean-up
+rm(county_data, county_subset, county_points, county_day_points, 
+   county_focal)
+rm(top_val, cols, p)
--- source/workflow/15_county_plots_spring.R
+++ source/workflow/15_county_plots_spring.R
@@ -9,22 +9,12 @@ county_data <- read_csv("data/MO_HEALTH_Covid_Tracking/data/county/county_full.c
 
 # =============================================================================
 
-# define colors
-pal_a <- brewer.pal(n = 8, name = "Set1")
-pal_a[6] <- "#FFD60C"
-pal_b <- brewer.pal(n = 6, name = "Reds")
-pal_b <- pal_b[c(6)]
-pal_c <- brewer.pal(n = 6, name = "Blues")
-pal_c <- pal_c[c(6)]
-pal <- c(pal_a, pal_b, pal_c)
-
-# clean-up
-rm(pal_a, pal_b, pal_c)
-
 # define cols object
-cols <- c("St. Louis City" = pal[1], "St. Louis" = pal[2], "Kansas City" = pal[3],
-          "Christian" = pal[4], "Greene" = pal[5], "Polk" = pal[7], "Taney" = pal[8],
-          "Stone" = pal[9], "Webster" = pal[6])
+cols <- c("St. Louis City" = values$pal[1], "St. Louis" = values$pal[2], 
+          "Kansas City" = values$pal[3], "Christian" = values$pal[4], 
+          "Greene" = values$pal[5], "Polk" = values$pal[7], 
+          "Taney" = values$pal[8], "Stone" = values$pal[9], 
+          "Webster" = values$pal[6])
 
 # define focal metros
 county_focal <- c("29510", "29189", "29511", "29043", "29077", "29167", 
@@ -34,33 +24,17 @@ county_focal <- c("29510", "29189", "29511", "29043", "29077", "29167",
 
 # create points
 ## create end points
-county_points <- filter(county_data, report_date == date) %>%
+county_points <- filter(county_data, report_date == values$date) %>%
   filter(geoid %in% county_focal)
-
-## create reporting change points
-report_points <- filter(county_data, report_date == as.Date("2020-04-15")) %>%
-  filter(geoid %in% county_focal) %>%
-  mutate(text = ifelse(geoid == "29189", "reporting change on 15 Apr", NA))
-
-# =============================================================================
-
-# create line label
-report_line <- tibble(
-  date = as.Date("2020-04-15"),
-  case_rate = county_rate_pos,
-  mortality_rate = NA,
-  case_fatality_rate = NA,
-  text = "reporting change on 15 Apr"
-)
 
 # =============================================================================
 
 # plot confirmed rate
 ## subset data
-county_subset <- filter(county_data, report_date >= plot_date)
+county_subset <- filter(county_data, report_date >= values$plot_date)
 
 ## define top_val
-top_val <- round_any(x = max(county_subset$case_rate), accuracy = county_rate_val, f = ceiling)
+top_val <- round_any(x = max(county_subset$case_rate), accuracy = values$county_rate_val, f = ceiling)
 
 ## create factors
 county_subset <- mutate(county_subset, factor_var = fct_reorder2(county, report_date, case_rate))
@@ -72,21 +46,19 @@ p <- ggplot() +
   geom_point(county_points, mapping = aes(x = report_date, y = case_rate, color = factor_var), 
              size = 4, show.legend = FALSE) +
   gghighlight(geoid %in% county_focal, use_direct_label = FALSE, use_group_by = FALSE) +
-  # geom_vline(xintercept = as.Date("2020-04-15"), linetype="dotted", size = 1.25) + 
-  # geom_text_repel(data = report_line, mapping = aes(x = date, y = case_rate, label = text),
-  #                nudge_y = county_rate_x, nudge_x = county_rate_y, size = 5) +
   scale_colour_manual(values = cols, name = "County") +
-  scale_x_date(date_breaks = date_breaks_alt, date_labels = "%d %b") +
-  scale_y_continuous(limits = c(0,top_val), breaks = seq(0, top_val, by = county_rate_val)) + 
+  scale_x_date(date_breaks = values$date_breaks, date_labels = "%d %b") +
+  scale_y_continuous(limits = c(0,top_val), breaks = seq(0, top_val, by = values$county_rate_val)) + 
   labs(
     title = "Reported COVID-19 Cases by Select Missouri Counties",
-    subtitle = paste0("Springfield Focus\n", as.character(plot_date), " through ", as.character(date)),
+    subtitle = paste0("Springfield Focus\n", as.character(values$plot_date), " through ", 
+                      as.character(values$date)),
     x = "Date",
     y = "Rate per 1,000",
-    caption = caption_text_census
+    caption = values$caption_text_census
   ) +
   sequoia_theme(base_size = 22, background = "white") +
-  theme(axis.text.x = element_text(angle = x_angle))
+  theme(axis.text.x = element_text(angle = values$x_angle))
 
 ## save plot
 save_plots(filename = "results/high_res/county_spring/b_case_rate.png", plot = p, preset = "lg")
@@ -108,17 +80,9 @@ top_val <- round_any(x = max(county_subset$day), accuracy = 5, f = ceiling)
 ## identify max day
 county_subset %>%
   group_by(geoid) %>%
-  summarise(day = max(day)) %>%
+  summarise(day = max(day), .groups = "drop_last") %>%
   left_join(county_points, ., by = "geoid") %>%
   filter(county %in% unique(county_subset$county)) -> county_day_points
-
-## add day to report points
-county_subset %>%
-  select(county, report_date, day) %>%
-  left_join(report_points, ., by = c("county", "report_date")) %>%
-  filter(county %in% unique(county_subset$county)) -> report_day_points
-
-report_label <- filter(report_day_points, county == "St. Louis")
 
 ## create factors
 county_subset <- mutate(county_subset, factor_var = fct_reorder2(county, day, cases))
@@ -130,17 +94,14 @@ p <- ggplot(data = county_subset) +
   geom_point(county_day_points, mapping = aes(x = day, y = cases, color = factor_var), 
              size = 4, show.legend = FALSE) +
   gghighlight(geoid %in% county_focal, use_direct_label = FALSE, use_group_by = FALSE) +
-  # geom_point(report_day_points, mapping = aes(x = day, y = cases), size = 4, shape = 18) +
-  # geom_text_repel(data = report_label, mapping = aes(x = day, y = cases, label = text),
-  #                nudge_y = county_log_y, nudge_x = county_log_x, size = 5) +
   scale_colour_manual(values = cols, name = "County") +
-  scale_y_log10(limits = c(5, county_log_max), breaks = c(5,10,30,100,300,1000,3000,10000,30000), 
+  scale_y_log10(limits = c(5, values$county_log_max), breaks = c(5,10,30,100,300,1000,3000,10000,30000), 
                 labels = comma_format(accuracy = 1)) +
-  scale_x_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = date_breaks_log)) +
+  scale_x_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = values$date_breaks_log)) +
   labs(
     title = "Pace of COVID-19 Cases by Select Missouri Counties",
-    subtitle = paste0("Springfield Focus\n", "Current as of ", as.character(date)),
-    caption = caption_text,
+    subtitle = paste0("Springfield Focus\n", "Current as of ", as.character(values$date)),
+    caption = values$caption_text,
     x = "Days Since Fifth Case Reported",
     y = "Count of Reported Cases (Log)"
   ) +
@@ -152,7 +113,45 @@ save_plots(filename = "results/low_res/county_spring/c_case_log.png", plot = p, 
 
 # =============================================================================
 
+# plot confirmed rate
+
+## subset data
+county_subset <- filter(county_data, report_date >= values$plot_date) %>%
+  filter(geoid %in% county_focal)
+
+## define top_val
+top_val <- round_any(x = max(county_subset$case_avg_rate), accuracy = 10, f = ceiling)
+
+## create factors
+county_subset <- mutate(county_subset, factor_var = fct_reorder2(county, report_date, case_avg_rate))
+county_points <- mutate(county_points, factor_var = fct_reorder2(county, report_date, case_avg_rate))
+
+## create plot
+p <- ggplot(county_subset) +
+  geom_line(mapping = aes(x = report_date, y = case_avg_rate, color = factor_var), size = 2) +
+  geom_point(county_points, mapping = aes(x = report_date, y = case_avg_rate, color = factor_var), 
+             size = 4, show.legend = FALSE) +
+  gghighlight(geoid %in% county_focal, use_direct_label = FALSE, use_group_by = FALSE) +
+  scale_colour_manual(values = cols, name = "County") +
+  scale_x_date(date_breaks = values$date_breaks, date_labels = "%d %b") +
+  scale_y_continuous(limits = c(0,top_val), breaks = seq(0, top_val, by = 10)) + 
+  labs(
+    title = "Pace of New COVID-19 Cases in Select Missouri Counties",
+    subtitle = paste0("Springfield Focus\n",as.character(values$plot_date), " through ", as.character(values$date)),
+    x = "Date",
+    y = "7-Day Average Rate per 100,000",
+    caption = values$caption_text_census
+  ) +
+  sequoia_theme(base_size = 22, background = "white") +
+  theme(axis.text.x = element_text(angle = values$x_angle))
+
+## save plot
+save_plots(filename = "results/high_res/county_spring/e_new_case.png", plot = p, preset = "lg")
+save_plots(filename = "results/low_res/county_spring/e_new_case.png", plot = p, preset = "lg", dpi = 72)
+
+# =============================================================================
+
 # clean-up
-rm(county_data, county_subset, county_points, county_day_points, report_day_points, report_points, 
-   report_label, report_line, county_focal, county_log_x, county_log_y)
-rm(top_val, pal, cols, p)
+rm(county_data, county_subset, county_points, county_day_points, 
+   county_focal)
+rm(top_val, cols, p)
