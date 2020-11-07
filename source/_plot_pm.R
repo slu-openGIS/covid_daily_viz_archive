31f856545f8474ca09ccec678d37aba6664fba9f
Christopher Prener <chris.prener@slu.edu>
2020-11-06 19:26:09 -0500

    build pm plots for 2020-11-06

--- source/_plot_pm.R
+++ source/_plot_pm.R
@@ -45,8 +45,8 @@ source("source/functions/sequoia_theme.R")
 source("source/workflow/03_state_individual_plots.R")
 
 # regional plots
-source("source/workflow/18_stl_zip_plots.R")
-source("source/workflow/18_stl_zip_plots_v2.R")
+# source("source/workflow/18_stl_zip_plots.R")
+# source("source/workflow/18_stl_zip_plots_v2.R")
 source("source/workflow/19_stl_individual_plots.R")
 source("source/workflow/20_stl_hospital_plots.R")
 
