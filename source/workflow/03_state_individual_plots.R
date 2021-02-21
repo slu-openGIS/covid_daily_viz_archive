9be80f57bd975b56c7f92ebc21e19acaaf36cc56
Christopher Prener <chris.prener@slu.edu>
2021-02-20 20:21:42 -0600

    build site for 2021-02-20

--- source/workflow/03_state_individual_plots.R
+++ source/workflow/03_state_individual_plots.R
@@ -106,3 +106,65 @@ save_plots(filename = "results/low_res/mo_individual/b_race_mortality.png", plot
 # clean-up
 rm(covid_race)
 rm(top_val, p)
+
+# =============================================================================
+
+# load data
+covid_race <-  read_csv("data/MO_HEALTH_Covid_Tracking/data/individual/mo_vaccine_race_rates.csv")
+
+# =============================================================================
+
+missing <- list(
+  race = covid_race %>%
+    filter(value %in% c("Unknown, Race")) %>%
+    select(total_dose) %>%
+    pull(),
+  ethnic = covid_race %>%
+    filter(value %in% c("Unknown, Ethnicity")) %>%
+    select(total_dose) %>%
+    pull(),
+  total = covid_race %>%
+    filter(value %in% c("Unknown, Ethnicity", "Latino") == FALSE) %>%
+    group_by(geoid) %>%
+    summarise(total = sum(total_dose)) %>%
+    select(total) %>%
+    pull()
+)
+
+covid_race <- filter(covid_race, value %in% c("Unknown, Race", "Unknown, Ethnicity", "Two or More") == FALSE)
+
+# =============================================================================
+
+# create factor
+covid_race <- covid_race %>%
+  mutate(value = case_when(
+    value == "Native" ~ "Native American",
+    TRUE ~ as.character(value)
+  ))
+
+# =============================================================================
+
+# plot total dose rates, race
+
+## define top_val
+top_val <- round_any(x = max(covid_race$total_dose_rate, na.rm = TRUE), accuracy = 2500, f = ceiling)
+
+## create plot
+p <- ggplot(data = covid_race, mapping = aes(x = reorder(value, -total_dose_rate), y = total_dose_rate)) +
+  geom_bar(position = "dodge", stat = "identity", width = .65, show.legend = FALSE,
+           fill = RColorBrewer::brewer.pal(4, "Set1")[4]) +
+  scale_y_continuous(limits = c(0, top_val), breaks = seq(0, top_val, by = 2500)) +
+  labs(
+    title = "Vaccinations by Race and Ethnicity, Missouri",
+    subtitle = paste0("Current as of ", as.character(date)),
+    x = "Race",
+    y = "Total Doses per 100,000 Individuals",
+    caption = paste0("Plot by Christopher Prener, Ph.D.\nData via the State of Missouri and the U.S. Census Bureau",
+                     "\nStatewide, ", round(missing$race/missing$total*100, 2),"% of race data are missing, as are ", 
+                     round(missing$ethnic/missing$total*100, 2), "% of ethnicity data")  
+  ) +
+  sequoia_theme(base_size = 22, background = "white", map = FALSE)
+
+## save plot
+save_plots(filename = "results/high_res/mo_individual/c_race_vaccine_total.png", plot = p, preset = "lg")
+save_plots(filename = "results/low_res/mo_individual/c_race_vaccine_total.png", plot = p, preset = "lg", dpi = 72)
